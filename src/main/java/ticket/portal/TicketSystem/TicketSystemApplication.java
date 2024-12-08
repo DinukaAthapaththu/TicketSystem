e660e1112cd9a8a53df5e7dfded2dcecaa173614
DinukaAthapaththu <dinukanasada2003@gmail.com>
2024-12-08 14:31:51 +0530

    init commit

---
+++ src/main/java/ticket/portal/TicketSystem/TicketSystemApplication.java
@@ -0,0 +1,13 @@
+package ticket.portal.TicketSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketSystemApplication.class, args);
+	}
+
+}
